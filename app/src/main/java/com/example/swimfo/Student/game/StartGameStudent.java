460d8f56740682a781ab224c146ef1b876f1fcde
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-11-25 09:21:27 +0800

    WebVIew Overview

--- app/src/main/java/com/example/swimfo/Student/game/StartGameStudent.java
+++ app/src/main/java/com/example/swimfo/Student/game/StartGameStudent.java
@@ -2,17 +2,33 @@ package com.example.swimfo.Student.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.swimfo.R;
+import com.google.android.material.button.MaterialButton;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -37,6 +53,7 @@ public class StartGameStudent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+        showDialogBox();
         game();
 
     }
@@ -149,10 +166,19 @@ public class StartGameStudent extends AppCompatActivity {
     }
     private void game() {
         myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://www.y8.com/games/swimming_hero/mobile");
-
         WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // This JavaScript code will remove the element with the ID 'MobileAdInGamePreroll-Box-Body'
+                myWebView.evaluateJavascript("document.getElementById('MobileAdInGamePreroll-Box-Body').style.display='none';", null);
+            }
+        });
+
+        myWebView.loadUrl("https://cdn-factory.marketjs.com/en/swimming-hero/index.html");
     }
 
     private Runnable scrollRunnable = new Runnable() {
@@ -168,6 +194,35 @@ public class StartGameStudent extends AppCompatActivity {
             handler.postDelayed(this, 10);
         }
     };
+
+
+    private void showDialogBox(){
+
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_game_mechanics);
+
+        Button btnOkay = dialog.findViewById(R.id.btn_okay);
+
+        btnOkay.setOnClickListener(v -> {
+            myWebView.evaluateJavascript("javascript:MobileAdInGamePreroll.Close();", null);
+            dialog.dismiss();
+        });
+
+        // Get the Window object of the dialog
+        Window window = dialog.getWindow();
+        if (window != null) {
+            // Set the layout parameters to match the window size
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            // Optional: Remove dialog background to make it full screen
+            window.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        }
+
+
+
+        dialog.show();
+
+    }
 
 
 }
